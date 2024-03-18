4d146438d9ec9783f18996306d27652b5616f3be
Aaron Wu <awu0626@seas.upenn.edu>
2024-03-18 13:20:15 -0400

    Delete src directory

--- src/pages/AboutUs/LeadershipPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LeadershipPage() {
-    return (
-        <div>
-            leadership
-        </div>
-    );
-}
--- src/pages/AboutUs/MissionPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MissionPage() {
-    return (
-        <div>
-            mission
-        </div>
-    );
-}
